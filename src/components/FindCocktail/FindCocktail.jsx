57ca264c586ccec9ec6cfbd6b4bab0fc00c23721
JonobaneL <jonobane@gmail.com>
2023-02-05 17:22:52 +0200

    Create function to clear input and hide searched results

--- src/components/FindCocktail/FindCocktail.jsx
+++ src/components/FindCocktail/FindCocktail.jsx
@@ -1,28 +1,43 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import styles from './FindCocktail.module.css'
 import CInput from "../UI/input/cInput";
 import { useFetching } from "../../hooks/useFetching";
 import CocktailService from "../../API/CocktailService";
-import DrinkCard from "../DrinkCard/DrinkCard";
 import DrinkList from "../DrinkList/DrinkList";
+import findIcon from '../../images/search-icon.png'
+import cancelIcon from '../../images/cancel.png'
+// import Filters from "../Filters/Filters";
 
 const FindCocktail =()=>{
-    const [fCocktail,setFCocktail] =useState('');
+    const [search,setSearch] =useState('');
     const [cocktails, setCocktails] = useState([]);
+    const [isShow, setIsShow] =useState(false)
     const [fetcCocktails,isCocktailsLoading,cocktailsError] = useFetching(async()=>{
-        const response = await CocktailService.getCocktailByName(fCocktail);
+        const response = await CocktailService.getCocktailByName(search);
         console.log(response.data.drinks)
         setCocktails(response.data.drinks);
     })
+
     const getData=()=>{
         fetcCocktails();
+        setIsShow(true);
         console.log(cocktails);
+    }
+    const clearData=()=>{
+        setIsShow(false);
+        setSearch('');
     }
     return(
         <div className={styles.find}>
-            <CInput value={fCocktail} onChange={e=>setFCocktail(e.target.value)} />
-            <button onClick={getData}>Find</button>
-            <DrinkList drinks={cocktails}/>
+            {/* вирішити як обновити стейт після натиску на кнопку в компоненті Fiter */}
+            {/* <Filters search={search} setSearch={setFCocktail} getData={getData}/> */}
+            <div className={styles.filter}>
+                <CInput value={search} onChange={e=>setSearch(e.target.value)} />
+                {isShow&&<button className={styles.cancelButton} onClick={clearData}><img src={cancelIcon} alt="Cancel" /></button>}
+                <button className={styles.findButton} onClick={getData}><img src={findIcon} alt="Find" /></button>
+            </div>
+            
+            {isShow &&(isCocktailsLoading ? <h1>Loading</h1>:<DrinkList drinks={cocktails}/>)}
         </div>
     )
 }

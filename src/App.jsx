e7def783268f88e9ea0cbfbcbb54d6a562648041
JonobaneL <jonobane@gmail.com>
2023-02-05 17:16:32 +0200

    Add Footer component to BrouserRote

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import CocktailService from './API/CocktailService'
 import './App.css'
 import AppRoute from './components/AppRouter/AppRouter'
-import FindCocktail from './components/FindCocktail/FindCocktail'
+import Footer from './components/UI/footer/Footer'
 import Navbar from './components/UI/navbar/Navbar'
-import { useFetching } from './hooks/useFetching'
 
 function App() {
-  const [cocktail,setCocktail] = useState({});
 
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar/>
         <AppRoute/>
+        <Footer/>
       </BrowserRouter>
     </div>
   )

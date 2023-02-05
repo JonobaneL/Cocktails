cf5e712d220176d9b36abde64f41a8ad56c08b79
JonobaneL <jonobane@gmail.com>
2023-02-05 17:13:32 +0200

    Add created components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CocktailService from "../API/CocktailService";
+import DrinkList from "../components/DrinkList/DrinkList";
 import FindCocktail from "../components/FindCocktail/FindCocktail";
+import PopularDrinks from "../components/PopularDrinks/PopularDrinks";
 
 const Home =()=>{
+    const [populaDrinks,setPopularDrinks] = useState([])
+    useEffect(()=>{
+        let temporalVar = CocktailService.getPopularCocktails();
+        temporalVar.then(res=>setPopularDrinks(res))
+    },[])
+    console.log(populaDrinks)
     return(
         <div>
             <FindCocktail />
+            <PopularDrinks />
         </div>
     )
 }

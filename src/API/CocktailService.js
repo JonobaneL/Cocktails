d0a9ceed66447fae6de12477485fb11ce9ef356f
JonobaneL <jonobane@gmail.com>
2023-02-05 17:06:26 +0200

    Modified getPopularCocktails function in return we get array with random cocktail

--- src/API/CocktailService.js
+++ src/API/CocktailService.js
@@ -2,8 +2,13 @@ import axios from 'axios'
 
 export default class CocktailService{
     static async getPopularCocktails (){
-        const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php');
-        return response;
+        const index=6;
+        let drinks = [];
+        for (let i=0;i<index;i++){
+            const responce = await axios('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+            drinks.push(...(responce.data.drinks))
+        }
+        return drinks;
     }
     static async getCocktailById(id){
         const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
